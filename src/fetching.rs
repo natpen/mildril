96f4c207032a1c600bd040e14324f3d6c17a8520
Natalie Pendragon <natalie.pendragon@gmail.com>
2016-10-24 22:20:27 -0400

    Don't explode when a url can't be fetched

--- src/fetching.rs
+++ src/fetching.rs
@@ -7,12 +7,12 @@ use self::hyper::Client;
 pub fn fetch_url(client: &Client, url: &String) -> String {
     let mut response = match client.get(url).send() {
         Ok(response) => response,
-        Err(_) => panic!("Error getting {}", url),
+        Err(_) => return String::new(),
     };
     let mut buf = String::new();
     match response.read_to_string(&mut buf) {
         Ok(_) => (),
-        Err(_) => panic!("Error reading response from {}", url),
+        Err(_) => return String::new(),
     };
     buf
 }
